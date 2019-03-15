270986671cc52ef9946e3d135da7582b01a0194e
solomon2773 <solomon.tsao@utdallas.edu>
2019-03-15 16:10:13 -0500

    Update nodejs_install.sh

--- nodejs_install.sh
+++ nodejs_install.sh
@@ -1,4 +1,7 @@
 #for centos 7
+#https://github.com/nodesource/distributions#rpminstall
 curl -sL https://rpm.nodesource.com/setup_11.x | bash -
-sudo yum install -y nodejs npm
+sudo yum install -y nodejs
 sudo yum install -y gcc-c++ make
+node -v
+npm -v
